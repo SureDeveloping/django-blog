7b0ac26a4e5dffc92ac6756a98644e5eb571c5f0
Stephan Sure <stephan.sure@gmail.com>
2024-04-06 09:53:57 +0000

    Change footer

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-n88op8(v+6@0ne63vmg7-y#6new)h4e6gxj%*yk$h_%(mq2gez'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-suredevelopi-djangoblog-ycxvm6pb7ve.ws-eu110.gitpod.io',
                 '.herokuapp.com']
