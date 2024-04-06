a55a086bb48a685bda540359fcb1f5659be2305b
Stephan Sure <stephan.sure@gmail.com>
2024-04-06 15:40:08 +0000

    Fix bug in index.html

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,10 +24,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n88op8(v+6@0ne63vmg7-y#6new)h4e6gxj%*yk$h_%(mq2gez'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-suredevelopi-djangoblog-ycxvm6pb7ve.ws-eu110.gitpod.io',
                 '.herokuapp.com']
