8fafb9bce3a551f119320aa8cc59b18c6960ed9f
Stephan Sure <stephan.sure@gmail.com>
2024-04-19 13:56:11 +0000

    Add cloudinary

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Flase
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-suredevelopi-djangoblog-ycxvm6pb7ve.ws-eu110.gitpod.io',
                 '.herokuapp.com']
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'blog',
     'about',
 ]
