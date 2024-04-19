5a52c1d9ad901d55f58de9b1f56feae6e262c51f
Stephan Sure <stephan.sure@gmail.com>
2024-04-19 09:57:03 +0000

    Add delete button

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = Flase
 
 ALLOWED_HOSTS = ['8000-suredevelopi-djangoblog-ycxvm6pb7ve.ws-eu110.gitpod.io',
                 '.herokuapp.com']
