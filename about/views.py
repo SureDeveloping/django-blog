5956fd70b4292fb7ac731183e47120e419fe68e1
Stephan Sure <stephan.sure@gmail.com>
2024-04-19 17:49:30 +0000

    Add comments to classes and models

--- about/views.py
+++ about/views.py
@@ -5,14 +5,25 @@ from .forms import CollaborateForm
 
 
 def about_me(request):
-
+    """
+    Render the most recent information on the website author and
+    allows User collaboration requests
+    Displays an inddividual instance of :model:`about.About`.
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`.
+    ``collobotate_form`` 
+        An instance of :form:`about.CollaborateForm`.
+    **Template:**
+    :template:`about/about.html`
+    """  
+    about = About.objects.all().order_by('-updated_on').first()
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
             collaborate_form.save()
             messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
 
-    about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
 
     return render(
